a9d015b9ff3206e78105f1ee39b8c70a40e2c104
SSP6258 <ssp6258@yahoo.com.tw>
2019-09-20 07:26:54 +0800

    Git Test Version 1

---
+++ Julia_study_01.jl
@@ -0,0 +1,15 @@
+#=
+Julia_study_01:
+- Julia version: 1.2.0
+- Author: user
+- Date: 2019-09-18
+=#
+
+function fun_1()
+    Array_1 = ["Hi", "Julia"]
+    for i in Array_1
+        println(i)
+    end
+end
+
+fun_1()
